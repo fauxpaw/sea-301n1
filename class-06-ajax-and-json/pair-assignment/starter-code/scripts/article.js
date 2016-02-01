837d593ba49ad4ce189d18a98ba3ce014cde8221
Michael <fauxpawsc@live.com>
2016-01-31 23:43:00 -0800

    finally got this working. thx windows slashes... and Obama of course

--- class-06-ajax-and-json/pair-assignment/starter-code/scripts/article.js
+++ class-06-ajax-and-json/pair-assignment/starter-code/scripts/article.js
@@ -37,7 +37,10 @@ Article.loadAll = function(rawData) {
   });
 
   rawData.forEach(function(ele) {
-    Article.all.push(new Article(ele));
+    // Article.all.push(new Article(ele));
+    Article.all = rawData.map(function(ele){
+      return new Article(ele);
+    });
   })
 }
 
@@ -48,15 +51,19 @@ Article.fetchAll = function() {
     // When rawData is already in localStorage,
     // we can load it with the .loadAll function above,
     // and then render the index page (using the proper method on the articleView object).
-    Article.loadAll(//TODO: What do we pass in here to the .loadAll function?
-    );
-    articleView.someFunctionToCall; //TODO: What method do we call to render the index page?
+    //DONE: What do we pass in here to the .loadAll function?
+    Article.loadAll(JSON.parse(localStorage.rawData));
+    articleView.initIndexPage(); //DONE: What method do we call to render the index page?
   } else {
     // TODO: When we don't already have the rawData,
     // we need to retrieve the JSON file from the server with AJAX (which jQuery method is best for this?),
     // cache it in localStorage so we can skip the server call next time,
     // then load all the data into Article.all with the .loadAll function above,
     // and then render the index page.
-
+    $.getJSON('data/hackerIpsum.json', function(data){
+        Article.loadAll(data);
+        localStorage.rawData = JSON.stringify(data);
+        articleView.initIndexPage();
+    });
   }
-}
+};
